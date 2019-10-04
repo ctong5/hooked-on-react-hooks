3a9c8d7b3b2ee2b97930cb0d3b353146bfa3d5af
ctong5 <cassandra.tong5@gmail.com>
2019-10-04 00:06:30 -0700

    Refactor to note taking app using hooks

--- react-new-features/src/index.js
+++ react-new-features/src/index.js
@@ -2,31 +2,60 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-const App = (props) => {
-  // state does not need to be an object
-  // can call useState as needed
-  // when using useState and update the state, it completely replaces the state
-  // use state returns array of [currentStateValue, function to update state]
-  // const array = useState(10);
-  const [count, setCount] = useState(props.count); // destructured
-  const [text, setText] = useState('');
+// const App = (props) => {
+//   // state does not need to be an object
+//   // can call useState as needed
+//   // when using useState and update the state, it completely replaces the state
+//   // use state returns array of [currentStateValue, function to update state]
+//   // const array = useState(10);
+//   const [count, setCount] = useState(props.count); // destructured
+//   const [text, setText] = useState('');
   
+//   return (
+//     <div>
+//       <p>The current {text || 'count'} is {count}</p>
+//       <button onClick={() => setCount(count - 1)}>-1</button>
+//       <button onClick={() => setCount(props.count)}>Reset</button>
+//       <button onClick={() => setCount(count + 1)}>+1</button>
+//       <input type="text" value={text} onChange={(e) => setText(e.target.value)}/>
+//     </div>
+//   );
+// };
+
+// App.defaultProps = {
+//   count: 0,
+// }
+
+const NoteApp = () => {
+  const [notes, setNotes] = useState([]);
+  const [title, setTitle] = useState('');
+  const addNote = (e) => {
+    e.preventDefault();
+    setNotes([
+      ...notes, 
+      { title }
+    ]);
+    setTitle('');
+  }
+
   return (
     <div>
-      <p>The current {text || 'count'} is {count}</p>
-      <button onClick={() => setCount(count - 1)}>-1</button>
-      <button onClick={() => setCount(props.count)}>Reset</button>
-      <button onClick={() => setCount(count + 1)}>+1</button>
-      <input type="text" value={text} onChange={(e) => setText(e.target.value)}/>
+      <h1>NOTES</h1>
+      {notes.map((note) => (
+        <div key={note.title}>
+          <h3>{note.title}</h3>
+        </div>
+      ))}
+      <p>Add note</p>
+      <form onSubmit={addNote}>
+        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+        <button>Add note</button>
+      </form>
     </div>
   );
 };
 
-App.defaultProps = {
-  count: 0,
-}
-
-ReactDOM.render(<App count={2} />, document.getElementById('root'));
+ReactDOM.render(<NoteApp />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
