14466040941851545d791ca583deaade2e052b89
ctong5 <cassandra.tong5@gmail.com>
2019-10-03 23:45:18 -0700

    Pass in initial count as props and add decrement button

--- react-new-features/src/index.js
+++ react-new-features/src/index.js
@@ -2,20 +2,26 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-const App = () => {
+const App = (props) => {
   // use state returns array of [currentStateValue, function to update state]
   // const array = useState(10);
-  const [count, setCount] = useState(10); // destructured
+  const [count, setCount] = useState(props.count); // destructured
 
   return (
     <div>
       <p>The current count is {count}</p>
+      <button onClick={() => setCount(count - 1)}>-1</button>
       <button onClick={() => setCount(count + 1)}>+1</button>
+
     </div>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+App.defaultProps = {
+  count: 0,
+}
+
+ReactDOM.render(<App count={2} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
