435c5247457b810f5ba799592437d44b50910a84
ctong5 <cassandra.tong5@gmail.com>
2019-10-03 23:38:33 -0700

    Refactor setCount to invoke in onClick attribute

--- react-new-features/src/index.js
+++ react-new-features/src/index.js
@@ -6,14 +6,11 @@ const App = () => {
   // use state returns array of [currentStateValue, function to update state]
   // const array = useState(10);
   const [count, setCount] = useState(10); // destructured
-  const increment = () => {
-    setCount(count + 1);
-  }
-  
+
   return (
     <div>
       <p>The current count is {count}</p>
-      <button onClick={increment}>+1</button>
+      <button onClick={() => setCount(count + 1)}>+1</button>
     </div>
   );
 };
