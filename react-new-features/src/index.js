5d2b85ed54d089f678fd951d744a920aa111d9cb
ctong5 <cassandra.tong5@gmail.com>
2019-10-03 23:36:45 -0700

    Define setCount to update state

--- react-new-features/src/index.js
+++ react-new-features/src/index.js
@@ -5,11 +5,15 @@ import * as serviceWorker from './serviceWorker';
 const App = () => {
   // use state returns array of [currentStateValue, function to update state]
   // const array = useState(10);
-  const [count] = useState(10); // destructured
-
+  const [count, setCount] = useState(10); // destructured
+  const increment = () => {
+    setCount(count + 1);
+  }
+  
   return (
     <div>
       <p>The current count is {count}</p>
+      <button onClick={increment}>+1</button>
     </div>
   );
 };
