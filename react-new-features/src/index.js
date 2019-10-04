30e08b4a1cbececae66c42809862a6f5bf7656d1
ctong5 <cassandra.tong5@gmail.com>
2019-10-03 23:35:24 -0700

    Import and invoke hooks useState method

--- react-new-features/src/index.js
+++ react-new-features/src/index.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<div>my new content</div>, document.getElementById('root'));
+const App = () => {
+  // use state returns array of [currentStateValue, function to update state]
+  // const array = useState(10);
+  const [count] = useState(10); // destructured
+
+  return (
+    <div>
+      <p>The current count is {count}</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
